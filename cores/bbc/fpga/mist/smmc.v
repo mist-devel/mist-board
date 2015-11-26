edfa1da2abc77c4016b746882d7036634203a33a
Till Harbaum <till@harbaum.org>
2015-11-26 11:02:42 +0100

    [BBC] Added missing rom and adc reference in project file

--- cores/bbc/fpga/mist/smmc.v
+++ cores/bbc/fpga/mist/smmc.v
@@ -84,9 +84,9 @@ module smmc (
 		altsyncram_component.clock_enable_input_a = "BYPASS",
 		altsyncram_component.clock_enable_output_a = "BYPASS",
 `ifdef NO_PLI
-		altsyncram_component.init_file = "../../../../bbc/roms/SUPERMMC.rif"
+		altsyncram_component.init_file = "../../roms/smmc.rif"
 `else
-		altsyncram_component.init_file = "../../../../bbc/roms/SUPERMMC.hex"
+		altsyncram_component.init_file = "../../roms/smmc.hex"
 `endif
 ,
 		altsyncram_component.intended_device_family = "Cyclone III",
@@ -123,7 +123,7 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 // Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
-// Retrieval info: PRIVATE: MIFfilename STRING "../../../../bbc/roms/SUPERMMC.hex"
+// Retrieval info: PRIVATE: MIFfilename STRING "../../roms/smmc.hex"
 // Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "16384"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
@@ -138,7 +138,7 @@ endmodule
 // Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
-// Retrieval info: CONSTANT: INIT_FILE STRING "../../../../bbc/roms/SUPERMMC.hex"
+// Retrieval info: CONSTANT: INIT_FILE STRING "../../roms/smmc.hex"
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone III"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
