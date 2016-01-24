d5587f6cb056a8702966b777de197217786b299c
Till Harbaum <till@harbaum.org>
2016-01-24 11:59:54 +0100

    [TG68K] Musashi bitfield fix

--- tests/tg68k/Musashi/m68k_in.c
+++ tests/tg68k/Musashi/m68k_in.c
@@ -2878,9 +2878,9 @@ M68KMAKE_OP(bfins, 32, ., .)
 		if((width + offset) > 32)
 		{
 			mask_byte = MASK_OUT_ABOVE_8(mask_base) << (8-offset);
-			insert_byte = MASK_OUT_ABOVE_8(insert_base);
+			insert_byte = MASK_OUT_ABOVE_8(insert_base) << (8-offset);
 			data_byte = m68ki_read_8(ea+4);
-			FLAG_Z |= (data_byte & mask_byte);
+			FLAG_Z |= (insert_byte & mask_byte);
 			m68ki_write_8(ea+4, (data_byte & ~mask_byte) | insert_byte);
 		}
 		return;
