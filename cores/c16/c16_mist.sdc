2529968f186948df17d2178b815c04aaa012a2dd
Gyorgy Szombathelyi <gyurco@freemail.hu>
2019-02-14 12:44:52 +0100

    [C16] Reconfigurable PLL for PAL/NTSC modes

--- cores/c16/c16_mist.sdc
+++ cores/c16/c16_mist.sdc
@@ -33,21 +33,22 @@ derive_pll_clocks -create_base_clocks
 derive_clock_uncertainty
 
 # Clock groups
-set_clock_groups -asynchronous -group [get_clocks {SPI_SCK}] -group [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[*]}]
-set_clock_groups -asynchronous -group [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[1]}] -group [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[0]}]
-set_clock_groups -asynchronous -group [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[1]}] -group [get_clocks {pll_ntsc|altpll_component|auto_generated|pll1|clk[0]}]
-
+set_clock_groups -asynchronous -group [get_clocks {SPI_SCK}] -group [get_clocks {pll_c1541|altpll_component|auto_generated|pll1|clk[*]}]
+set_clock_groups -asynchronous -group [get_clocks {SPI_SCK}] -group [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[*]}]
+set_clock_groups -asynchronous -group [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[*]}] -group [get_clocks {pll_c1541|altpll_component|auto_generated|pll1|clk[*]}]
 
 # Some relaxed constrain to the VGA pins. The signals should arrive together, the delay is not really important.
-set_output_delay -clock [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[0]}] -max 0 [get_ports {VGA_*}]
-set_output_delay -clock [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[0]}] -min -5 [get_ports {VGA_*}]
+set_output_delay -clock [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[0]}] -max 0 [get_ports {VGA_*}]
+set_output_delay -clock [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[0]}] -min -5 [get_ports {VGA_*}]
 
 # SDRAM delays
-set_input_delay -clock [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[0]}] -max 6.4 [get_ports SDRAM_DQ[*]]
-set_input_delay -clock [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[0]}] -min 3.2 [get_ports SDRAM_DQ[*]]
+set_input_delay -clock [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[0]}] -max 6.4 [get_ports SDRAM_DQ[*]]
+set_input_delay -clock [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[0]}] -min 3.2 [get_ports SDRAM_DQ[*]]
 
-set_output_delay -clock [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[0]}] -max 1.5 [get_ports {SDRAM_D* SDRAM_A* SDRAM_BA* SDRAM_n* SDRAM_CKE}]
-set_output_delay -clock [get_clocks {pll_pal|altpll_component|auto_generated|pll1|clk[0]}] -min -0.8 [get_ports {SDRAM_D* SDRAM_A* SDRAM_BA* SDRAM_n* SDRAM_CKE}]
+set_output_delay -clock [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[0]}] -max 1.5 [get_ports {SDRAM_D* SDRAM_A* SDRAM_BA* SDRAM_n* SDRAM_CKE}]
+set_output_delay -clock [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[0]}] -min -0.8 [get_ports {SDRAM_D* SDRAM_A* SDRAM_BA* SDRAM_n* SDRAM_CKE}]
+set_output_delay -clock [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[0]}] -max 1.5 [get_ports {SDRAM_CLK}]
+set_output_delay -clock [get_clocks {pll_c16|altpll_component|auto_generated|pll1|clk[0]}] -min -0.8 [get_ports {SDRAM_CLK}]
 
 set_false_path -to [get_ports {AUDIO_L}]
 set_false_path -to [get_ports {AUDIO_R}]
--- cores/c16/c16_mist.v
+++ cores/c16/c16_mist.v
@@ -57,7 +57,6 @@ module c16_mist (
    output [5:0]  VGA_B
 );
 
-parameter MODE_PAL = 1'b1;
 // -------------------------------------------------------------------------
 // ------------------------------ user_io ----------------------------------
 // -------------------------------------------------------------------------
@@ -519,7 +518,7 @@ always @(negedge clk28) begin
 end
 
 // include the c16 itself
-C16 #(.MODE_PAL(MODE_PAL)) c16 (
+C16 c16 (
 	.CLK28   ( clk28 ),
 	.RESET   ( reset ),
 	.WAIT    ( c16_wait ),
@@ -571,8 +570,8 @@ C16 #(.MODE_PAL(MODE_PAL)) c16 (
 
 // the FPGATED uses two different clocks for NTSC and PAL mode.
 // Switching the clocks may crash the system. We might need to force a reset it.
-wire clk28 = MODE_PAL?clk28_pal:clk28_ntsc;
-wire pll_locked = pll_pal_locked && pll_ntsc_locked;
+wire pll_locked = pll_c1541_locked && pll_c16_locked;
+wire ntsc = ~c16_pal;
 
 // tv15hkz has quarter the pixel rate, so we need a 7mhz clock for the OSD
 reg clk7;
@@ -588,22 +587,127 @@ always @(posedge clk28) begin
 end
 
 // A PLL to derive the system clock from the MiSTs 27MHz
-wire clk32;
-wire pll_pal_locked, clk28_pal;
-pll_pal pll_pal (
-	 .inclk0( CLOCK_27        ),
-	 .c0(     clk28_pal       ),
-	 .c1(     clk32           ),
-	 .locked( pll_pal_locked  )
+wire pll_c1541_locked, clk32;
+pll_c1541 pll_c1541 (
+    .inclk0 ( CLOCK_27          ),
+    .c0     ( clk32             ),
+    .locked ( pll_c1541_locked  )
 );
 
-wire pll_ntsc_locked, clk28_ntsc;
-pll_ntsc pll_ntsc (
-	 .inclk0( CLOCK_27         ),
-	 .c0(     clk28_ntsc       ),
-	 .locked( pll_ntsc_locked  )
+wire pll_c16_locked, clk28;
+pll_c16 pll_c16 (
+    .inclk0(CLOCK_27),
+    .c0(clk28),
+    .areset(pll_areset),
+    .scanclk(pll_scanclk),
+    .scandata(pll_scandata),
+    .scanclkena(pll_scanclkena),
+    .configupdate(pll_configupdate),
+    .scandataout(pll_scandataout),
+    .scandone(pll_scandone),
+    .locked(pll_c16_locked)
 );
 
+wire       pll_reconfig_busy;
+wire       pll_areset;
+wire       pll_configupdate;
+wire       pll_scanclk;
+wire       pll_scanclkena;
+wire       pll_scandata;
+wire       pll_scandataout;
+wire       pll_scandone;
+reg        pll_reconfig_reset;
+wire [7:0] pll_rom_address;
+wire       pll_rom_q;
+reg        pll_write_from_rom;
+wire       pll_write_rom_ena;
+reg        pll_reconfig;
+wire       q_reconfig_ntsc;
+wire       q_reconfig_pal;
+
+rom_reconfig_pal rom_reconfig_pal
+(
+    .address(pll_rom_address),
+    .clock(clk32),
+    .rden(pll_write_rom_ena),
+    .q(q_reconfig_pal)
+);
+
+rom_reconfig_ntsc rom_reconfig_ntsc
+(
+    .address(pll_rom_address),
+    .clock(clk32),
+    .rden(pll_write_rom_ena),
+    .q(q_reconfig_ntsc)
+);
+
+assign pll_rom_q = ntsc ? q_reconfig_ntsc : q_reconfig_pal;
+
+pll_reconfig pll_reconfig_inst
+(
+    .busy(pll_reconfig_busy),
+    .clock(clk32),
+    .counter_param(0),
+    .counter_type(0),
+    .data_in(0),
+    .pll_areset(pll_areset),
+    .pll_areset_in(0),
+    .pll_configupdate(pll_configupdate),
+    .pll_scanclk(pll_scanclk),
+    .pll_scanclkena(pll_scanclkena),
+    .pll_scandata(pll_scandata),
+    .pll_scandataout(pll_scandataout),
+    .pll_scandone(pll_scandone),
+    .read_param(0),
+    .reconfig(pll_reconfig),
+    .reset(pll_reconfig_reset),
+    .reset_rom_address(0),
+    .rom_address_out(pll_rom_address),
+    .rom_data_in(pll_rom_q),
+    .write_from_rom(pll_write_from_rom),
+    .write_param(0),
+    .write_rom_ena(pll_write_rom_ena)
+);
+
+always @(posedge clk32) begin
+    reg ntsc_d, ntsc_d2, ntsc_d3;
+    reg [1:0] pll_reconfig_state = 0;
+    reg [9:0] pll_reconfig_timeout;
+
+    ntsc_d <= ntsc;
+    ntsc_d2 <= ntsc_d;
+    pll_write_from_rom <= 0;
+    pll_reconfig <= 0;
+    pll_reconfig_reset <= 0;
+    case (pll_reconfig_state)
+    2'b00:
+    begin
+        ntsc_d3 <= ntsc_d2;
+        if (ntsc_d2 ^ ntsc_d3) begin
+            pll_write_from_rom <= 1;
+            pll_reconfig_state <= 2'b01;
+        end
+    end
+    2'b01: pll_reconfig_state <= 2'b10;
+    2'b10:
+        if (~pll_reconfig_busy) begin
+            pll_reconfig <= 1;
+            pll_reconfig_state <= 2'b11;
+            pll_reconfig_timeout <= 10'd1000;
+        end
+    2'b11:
+    begin
+        pll_reconfig_timeout <= pll_reconfig_timeout - 1'd1;
+        if (pll_reconfig_timeout == 10'd1) begin
+            // pll_reconfig stuck in busy state
+            pll_reconfig_reset <= 1;
+            pll_reconfig_state <= 2'b00;
+        end
+        if (~pll_reconfig & ~pll_reconfig_busy) pll_reconfig_state <= 2'b00;
+    end
+    default: ;
+    endcase
+end
 // ---------------------------------------------------------------------------------
 // ----------------------------------- floppy 1541 ---------------------------------
 // ---------------------------------------------------------------------------------
